5f3e92d0215560b48997aab6e46f22c8f8edf1c7
Jakob <j.maroufi@htlkrems.at>
2026-05-12 10:41:57 +0200

    Fixed a bug in Darkmode/Lightmode change

--- frontend/script.js
+++ frontend/script.js
@@ -7,14 +7,11 @@ button.addEventListener("click", () => {
         html.setAttribute("data-bs-theme", "light");
         button.textContent = "Dark Mode";
 
-        button.classList.remove("btn-outline-light");
-        button.classList.add("btn-outline-dark");
-
     } else {
         html.setAttribute("data-bs-theme", "dark");
         button.textContent = "Light Mode";
 
-        button.classList.remove("btn-outline-dark");
-        button.classList.add("btn-outline-light");
+        // button.classList.remove("btn-outline-dark");
+        // button.classList.add("btn-outline-light");
     }
 });
